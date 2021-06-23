2f82879fcf932306c1d2b2dd109b8f3bd014698d
Mr-SteveNascimento <stevenson.melo.silva07@aluno.ifce.edu.br>
2021-06-23 19:04:44 -0300

    Adicionando formik-antd

--- src/components/form.js
+++ src/components/form.js
@@ -1,13 +1,65 @@
 import React, { useState } from 'react'
 import { Collapse, Descriptions, Button } from 'antd';
+import { Formik, useField } from 'formik'
+import { Form, Input, DatePicker } from 'formik-antd'
+import * as Yup from 'yup';
 import './form.css'
 import * as Service from '../services/submit-service'
 
 const { Panel } = Collapse;
 
+const MyTextInput = ({...props}) => {
+    const [field, meta] = useField(props)
+    return (
+        <>
+            <Input {...field} {...props}/>
+            {meta.touched && meta.error ? (
+                <div className="error">{meta.error}</div>
+            ) : null}
+        </>
+    )
+}
 
+const MyDateInput = ({...props}) => {
+    const meta = useField(props)[1]
+    return (
+        <>
+            <DatePicker {...props} />
+            {meta.touched && meta.error ? (
+                <div className="error">{meta.error}</div>
+            ) : null}
+        </>
+    )
+}
 
-export default function Form() {
+const formSchema = Yup.object().shape({
+    nomePac: Yup.string()
+        .min(2, 'Muito Curto!')
+        .max(50, 'Muito Longo!')
+        .required('Obrigatório!'),
+    dataNascPac: Yup.string()
+        .required('Obrigatório!').nullable('Obrigatório!'),
+    sintomasPac: Yup.string()
+        .min(2, 'Muito Curto!')
+        .max(50, 'Muito Longo!')
+        .required('Obrigatório!'),
+    inicioSintPac: Yup.string()
+    .required('Obrigatório!').nullable('Obrigatório!'),
+    nomePai: Yup.string()
+        .min(2, 'Muito Curto!')
+        .max(50, 'Muito Longo!')
+        .required('Obrigatório!'),
+    dataNascPai: Yup.string()
+    .required('Obrigatório!').nullable('Obrigatório!'),
+    nomeMae: Yup.string()
+        .min(2, 'Muito Curto!')
+        .max(50, 'Muito Longo!')
+        .required('Obrigatório!'),
+    dataNascMae: Yup.string()
+    .required('Obrigatório!').nullable('Obrigatório!'),
+})
+
+export default function Forms() {
 
     const [sending, setSending] = useState(false);
 
@@ -15,8 +67,7 @@ export default function Form() {
         name:"Steve"
     }
     
-    const callback = (key) => {
-      console.log(key);
+    const changeForm = (key) => {
     }
     
     const submit = async () => {
@@ -27,30 +78,60 @@ export default function Form() {
     
     return (
         <div>
+            <Formik
+                initialValues={{
+                    nomePac: "",
+                    dataNascPac: null,
+                    sintomasPac: "",
+                    inicioSintPac: null,
+                    nomePai: "",
+                    dataNascPai: null,
+                    nomeMae: "",
+                    dataNascMae: null,
+                }}
+                validationSchema={formSchema}
+            >
+                <Form>
 
-            <Collapse defaultActiveKey={['1']} onChange={callback}>
-                <Panel header="Informações do Paciente" key="1">
-                    <Descriptions title="Paciente" bordered style={{padding:'10px'}}>
-                        <Descriptions.Item label="Nome">Stevenson Nascimento</Descriptions.Item>
-                        <Descriptions.Item label="Data de Nascimento"> 04 de Setembro de 2001</Descriptions.Item>
-                    </Descriptions>
-                    <Descriptions title="Diagnóstico" bordered style={{padding:'10px'}}>
-                        <Descriptions.Item label="Sintomas">Dor no corpo</Descriptions.Item>
-                        <Descriptions.Item label="Início dos sintomas"> 04 de Setembro de 2001</Descriptions.Item>
-                    </Descriptions>
-                </Panel>
-                <Panel header="Família do Paciente" key="2">
-                    <Descriptions title="Pais" bordered style={{padding:'10px'}}>
-                        <Descriptions.Item label="Nome do Pai">Francisco Cheurides Silva Nascimento</Descriptions.Item>
-                        <Descriptions.Item label="Data de Nascimento">28 de Janeiro de 1965</Descriptions.Item>
-                        <Descriptions.Item label="Nome da Mãe">Maria do Amparo Melo Nascimento</Descriptions.Item>
-                        <Descriptions.Item label="Data de Nascimento">10 de Fevereiro de 1964</Descriptions.Item>
-                    </Descriptions>
-                </Panel>
-            </Collapse>
-            <Button loading={sending} type="primary" className="submit" onClick={submit}>Submit</Button>
+                    <Collapse onChange={changeForm}>
+                            <Panel header="Informações do Paciente" key="1">
+                                <Descriptions title="Paciente" bordered style={{padding:'10px'}}>
+                                        <Descriptions.Item label="Nome">
+                                            <MyTextInput name="nomePac" placeholder="Nome" type="text"/>
+                                        </Descriptions.Item>
+                                        <Descriptions.Item label="Data de Nascimento">
+                                            <MyDateInput name="dataNascPac" placeholder="Data de Nascimento" />
+                                        </Descriptions.Item>
+                                </Descriptions>
+                                <Descriptions title="Diagnóstico" bordered style={{padding:'10px'}}>
+                                    <Descriptions.Item label="Sintomas">
+                                        <MyTextInput name="sintomasPac" placeholder="Nome" type="text"/>
+                                    </Descriptions.Item>
+                                    <Descriptions.Item label="Início dos sintomas">
+                                        <MyDateInput name="inicioSintPac" placeholder="Inicio dos sintomas" />
+                                    </Descriptions.Item>
+                                </Descriptions>
+                            </Panel>
+                            <Panel header="Família do Paciente" key="2">
+                                <Descriptions title="Pais" bordered style={{padding:'10px'}}>
+                                    <Descriptions.Item label="Nome do Pai" span={2}>
+                                        <MyTextInput name="nomePai" placeholder="Nome" type="text"/>
+                                    </Descriptions.Item>
+                                    <Descriptions.Item label="Data de Nascimento">
+                                        <MyDateInput name="dataNascPai" placeholder="Data de Nascimento" />
+                                    </Descriptions.Item>
+                                    <Descriptions.Item label="Nome da Mãe" span={2}>
+                                        <MyTextInput name="nomeMae" placeholder="Nome" type="text"/>
+                                    </Descriptions.Item>
+                                    <Descriptions.Item label="Data de Nascimento">
+                                        <MyDateInput name="dataNascMae" placeholder="Data de Nascimento" />
+                                    </Descriptions.Item>
+                                </Descriptions>
+                            </Panel>
+                    </Collapse>
+                </Form>
+            </Formik>
+            <Button loading={sending} type="primary" className="submit" onClick={submit}>Enviar</Button>
         </div>
-            
-
     )
 }
